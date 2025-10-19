818a74e8ed62c9f018940f302b8decf44f262eef
mohamedhishamcs-glitch <mohamedhishamcs@gmail.com>
2025-10-19 18:15:28 +0300

    Update scenarios.py

--- src/scenarios.py
+++ src/scenarios.py
@@ -153,6 +153,26 @@ _SCENARIOS: Dict[str, Tuple[List[Cone], CarPose]] = {
         ],
         CarPose(x=0.0, y=0.0, yaw=1.8),
     ),
+
+    "23": (
+        [
+            Cone(x=5.0, y=3.0, color=0),
+            Cone(x=5.0, y=1.0, color=0),
+            Cone(x=5.0, y=5.0, color=0),
+            Cone(x=3.0, y=5.0, color=1),
+            Cone(x=3.0, y=3.0, color=1),
+            Cone(x=3.0, y=1.0, color=1)
+        ],
+        CarPose(x=0.0, y=0.0, yaw=0.1),
+    ),
+
+    "24": (
+        [
+            Cone(x=4.0, y=3.0, color=0),
+            Cone(x=2.0, y=3.0, color=1)
+        ],
+        CarPose(x=0.0, y=0.0, yaw=1.8),
+    ),
 }
 
 
